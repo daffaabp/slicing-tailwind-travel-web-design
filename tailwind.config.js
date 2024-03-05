1332d8d833121375f8dbe9b0e7257d3c3f3b6525
daffaabp <daffa.budi2003@gmail.com>
2024-03-05 21:59:21 +0700

    resposive navbar in smartphone, tablet, and desktop

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,13 @@ module.exports = {
     extend: {
       fontFamily: {
         "inter" : ['inter'],
+        "Circular" : ['Circular STD'],
       },
       colors: {
         "ungu" : '#5D50C6',
         "pink" : '#F85E9F',
         "orange" : '#FF5722',
+        "grey" : '#222831',
       }
     },
   },
